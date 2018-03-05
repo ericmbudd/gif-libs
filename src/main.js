887c8d60ac1a5bb3150a492fd374ee0987e9d9d6
Eric Budd <ericbudd@gmail.com>
2018-03-05 13:25:50 -0700

    initial flow set up

--- src/main.js
+++ src/main.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
   let url = "https://api.giphy.com/v1/gifs/search?api_key=CTnSxefMIGBG1JxWvBr6zVvKSLu7FQAw&q=tease&limit=25&offset=0&rating=PG&lang=en"
   let savedSearch = {}
+  // $('#initial').hide('slow/400/fast', function() {});
+  // $('#picker').show('slow/400/fast', function() {});
+  // $('#story').hide('slow/400/fast', function() {});
+  // $("#image")[0].href = "";
+
+
+  function togglePicker() {
+    $('#initial').fadeOut('500', function() {});
+    setTimeout(function() {
+      $('#picker').fadeIn('500', function() {});
+    }, 500);
+  }
+
+  function toggleStory() {
+    $('#picker').fadeOut('500', function() {});
+    setTimeout(function() {
+      $('#story').fadeIn('500', function() {});
+    }, 500);
+  }
 
   if (localStorage.getItem("saved") !== null) {
     console.log("save exists");
 
     savedSearch = JSON.parse(localStorage.getItem("saved"))
     console.log(savedSearch);
+    console.log(document);
+    console.log($("#image"));
   } else {
     console.log("no save");
     $.getJSON(url, function(data) {
-      //console.log(data);
+      // console.log(data);
       localStorage.setItem("saved", JSON.stringify(data))
     });
   }
 
+  $('#create').click(togglePicker)
+  $('#picker').click(toggleStory)
 });
