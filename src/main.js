0cee99c28b2b2710a2bd3f987aaa25ec849519e3
Eric Budd <ericbudd@gmail.com>
2018-03-05 16:03:46 -0700

    get DOM appends working

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   let url = "https://api.giphy.com/v1/gifs/search?api_key=CTnSxefMIGBG1JxWvBr6zVvKSLu7FQAw&q=tease&limit=25&offset=0&rating=PG&lang=en"
   let savedSearch = {}
-  // $('#initial').hide('slow/400/fast', function() {});
-  // $('#picker').show('slow/400/fast', function() {});
+  $('#initial').hide('slow/400/fast', function() {});
+  $('#picker').show('slow/400/fast', function() {});
   // $('#story').hide('slow/400/fast', function() {});
   // $("#image")[0].href = "";
+  console.log($('#pickerRow'));
+  for (var i = 0; i < 12; i++) {
+    builderPickerLayout();
+  }
+
 
 
   function togglePicker() {
@@ -21,13 +26,34 @@ $(document).ready(function() {
     }, 500);
   }
 
+  function builderPickerLayout() {
+    // let newGifsSet = document.createElement("div")
+    // newGifsSet.classList.add('container')
+    // newGifsSet.classList.add('col-xl-3')
+    // newGifsSet.classList.add('justify-content-center')
+
+    let newiframe = document.createElement("iframe")
+    newiframe.setAttribute('src', 'https://giphy.com/embed/Q7Eezvahu2Hrq');
+    newiframe.setAttribute('width', '341');
+    newiframe.setAttribute('height', '480');
+    newiframe.setAttribute('frameBorder', '0');
+    newiframe.classList.add('giphy-embed')
+    newiframe.classList.add('justify-content-center')
+    newiframe.classList.add('col-xl-3')
+
+    // console.log("newGifsSet = " + newGifsSet);
+    console.log("newiframe = " + newiframe);
+
+    // newGifsSet.append(newiframe)
+    $('#pickerRow').append(newiframe)
+
+  }
+
   if (localStorage.getItem("saved") !== null) {
     console.log("save exists");
 
     savedSearch = JSON.parse(localStorage.getItem("saved"))
     console.log(savedSearch);
-    console.log(document);
-    console.log($("#image"));
   } else {
     console.log("no save");
     $.getJSON(url, function(data) {
